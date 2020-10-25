bec1dc612529a67e171abca82e238b45725638c8
Francois Lesage <48120561+storagebits@users.noreply.github.com>
2020-10-25 21:40:36 +0100

    added bluetooth setup

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-basic.forever(function () {
-	
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # .
+        `)
 })
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # . . # .
+        # . . # .
+        # # # . .
+        `)
+})
+basic.showLeds(`
+    # . . # #
+    # . . # #
+    # # # . .
+    # . # . .
+    # # # . .
+    `)
+bluetooth.startAccelerometerService()
+bluetooth.startButtonService()
+bluetooth.startLEDService()
+bluetooth.startTemperatureService()
