5281d4d4bfb972ce8beffa8b9167290cc62c18cd
Francois Lesage <francois.lesage@gmail.com>
2020-10-29 00:16:07 +0100

    fixed device addr and pybit call

--- mpybit.py
+++ mpybit.py
@@ -8,11 +8,11 @@ import subprocess
 # list of devices
 devices = [
   "DC:4A:04:CA:C3:6F",   # device #1
-  "DC:A6:32:5D:64:8D"   # device #2
+  "E2:31:3A:95:93:94"   # device #2
    ]
 
 for x in range(0,len(devices)):
-  cmd = "python pybit.py " + devices[x]
+  cmd = "./pybit.py " + devices[x]
   subprocess.call(cmd, shell=True)
 
 print("finished all devices!")
